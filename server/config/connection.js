f095268e577ce3867dfb029bc93b9d7c02323ee8
Sheri Foster <fostr.sheri@gmail.com>
2022-01-16 16:33:37 -0500

    "trying to deploy"

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redux', {
+mongoose.connect(process.env.MONGODB_URI || 'mongosh "mongodb+srv://redux.gppqy.mongodb.net/redux" --username sfoster71', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
